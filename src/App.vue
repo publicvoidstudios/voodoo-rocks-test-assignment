aae02415d9857f6a933e1ce491026809470fb554
Vladislav Pak <vlad.pak1996@gmail.com>
2024-03-18 17:25:41 +0200

    Code refactor. Bug fixes.
    - Added errors handling for fetch api methods.
    - Converted displayed posts to computed method (previously "shown" posts).
    - Replaced waitForPosts function with async and computed method.
    - Added visual representation of server errors.
    - Shortened code. Replaced fetchPosts and fetchUsers functions with a single fetchData function with an api url parameter.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container-fluid d-flex flex-column align-items-center">
-    <search-bar @searchInput="handleSearch"></search-bar>
-    <loading-spinner v-if="!shownPosts.length && !searchInput.length"></loading-spinner>
-    <post-list :posts="shownPosts" :users="users" :searchInput="searchInput"></post-list>
+    <search-bar @searchInput="handleInput" v-if="!errors.length"></search-bar>
+    <loading-spinner v-if="!displayedPosts.length && !searchInput.length && !errors.length"></loading-spinner>
+    <post-list :posts="displayedPosts" :users="users" :searchInput="searchInput" :errors="errors"></post-list>
   </div>
 </template>
 
@@ -18,58 +18,46 @@ export default {
     return {
       posts: [],
       users: [],
-      shownPosts: [],
-      searchInput: ''
+      searchInput: '',
+      errors: []
     }
   },
   methods: {
-    //Get posts from json-placeholder and store them in posts array
-    async fetchPosts() {
-      await fetch('https://jsonplaceholder.typicode.com/posts')
-          .then(response => response.json())
-          .then(json => this.posts = json);
-    },
-    //Get users from json-placeholder and store them in users array
-    async fetchUsers() {
-      await fetch('https://jsonplaceholder.typicode.com/users')
-          .then(response => response.json())
-          .then(json => this.users = json);
-    },
-    //Handle search input
-    handleSearch(input) {
-      //Assign search input string to searchInput to be able to use it further
-      this.searchInput = input;
-      //If search input is empty -> use all posts
-      if(input.trim() === ''){
-        this.useAllPosts();
-      } else {
-        //Filter posts using search input and store filtered posts, that are going to be shown, in shownPosts array
-        this.shownPosts = this.posts.filter(post => this.users.filter(user => user.name.toLowerCase().includes(input.toLowerCase())).some(user => user.id === post.userId));
+    // Method to get data from API URL and store it in a variable
+    async fetchData(url) {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data from ${url}`);
+        }
+        return await response.json();
+      } catch (error) {
+        console.error(`Error fetching data from ${url}. Error: ${error.message}`);
+        this.errors.push({
+          message: `Error fetching data from ${url}. Error: ${error.message}`
+        });
+        return error;
       }
     },
-    //Makes all posts visible by using all posts in shownPosts
-    useAllPosts() {
-      this.shownPosts = this.posts;
+
+    //Method to store input data received from event
+    handleInput(input) {
+      this.searchInput = input;
     },
+
     //Wait for all data to be received and then use it in app
     async readyData() {
-      try {
-        await this.fetchUsers();
-        await this.fetchPosts();
-        this.waitForPosts();
-      } catch (e) {
-        console.log(`Failed to retrieve data from server with error: ${e}`);
-      }
-    },
-    //Wait for posts to fill with data and use posts when ready
-    waitForPosts() {
-      //If posts are not empty
-      if (this.posts.length) {
-        //Use posts in app
-        this.useAllPosts();
+        this.posts = await this.fetchData('https://jsonplaceholder.typicode.com/posts');
+        this.users = await this.fetchData('https://jsonplaceholder.typicode.com/users');
+    }
+  },
+  computed: {
+    displayedPosts() {
+      if(this.searchInput.trim() === ''){
+        return this.posts;
       } else {
-        //Reset function with small timeout
-        setTimeout(this.waitForPosts, 100);
+        //Filter posts using search input and store filtered posts, that are going to be shown, in shownPosts array
+        return this.posts.filter(post => this.users.filter(user => user.name.toLowerCase().includes(this.searchInput.toLowerCase())).some(user => user.id === post.userId));
       }
     }
   },
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,7 @@
 <template>
   <div
       class="container-fluid"
-      v-if="users.length"
+      v-if="!errors.length && users.length && posts.length"
   >
     <div class="row">
       <post-item
@@ -20,6 +20,15 @@
       No matches found for "{{searchInput}}"
     </div>
 
+  </div>
+
+  <div
+    v-if="errors.length"
+    v-for="error in errors"
+    class="text-danger border border-2 border-danger rounded bg-dark text-start m-2 p-2"
+  >
+    <p>An error occurred when receiving data from server.</p>
+    <p class="text-light">{{error.message}}</p>
   </div>
 </template>
 
@@ -42,6 +51,11 @@ export default {
       type: String,
       required: true,
       default: ''
+    },
+    errors: {
+      type: Array,
+      default: [],
+      required: true
     }
   }
 }
